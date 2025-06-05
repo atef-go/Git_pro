28ec4a7a3c62efad9ac74f654f8031d657f7d9f5
atif <atifgo2022@gmail.com>
2025-06-05 00:08:23 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 
-// =========== start  === XMLHttpRequest ====
-( function getApi(){
-     var xhttp = new XMLHttpRequest;
-     xhttp.onreadystatechange = function(){
-         if(xhttp.readyState==4 && xhttp.status==200){
-         var items = xhttp.responseText;
-             var result =JSON.parse(items);
-// ===================================================
-             var ahmed =result.products;
-             var myText = "";
-             for(var i=0; i<ahmed.length; i++){
-                 var cartoona =`
-                                      <div class="col-md-4 text-center mb-5">
-                                      <img src="${ahmed[i].images[0]}" class='w-100' style='height:350px'>
-                                      <h4>${ahmed[i].title}</h4>
-                                      <h5>${ahmed[i].category}</h5>
-                                      <h5>${ahmed[i].price}$</h5>
-                                      </div>
-                                      ` ; 
-                  myText = myText+cartoona;
-              }
-               document.querySelector(".test").innerHTML=myText;        
-// ==================================================
-         }
-     }
-     xhttp.open("GET","https://dummyjson.com/products",true);
-     xhttp.send();
- })();
-// =========== End  === XMLHttpRequest ====
+// ========================= fetch ===    then   ===================
+
+ fetch("https://dummyjson.com/products")
+.then(function(items){
+    return items.json();
+})
+.then(function(result){
+// ==================================
+var ahmed =result.products;
+
+var myText = "";
+for(var i=0; i<ahmed.length; i++){
+
+    var cartoona =`
+                    <div class="col-md-4 text-center mb-5">
+                    <img src="${ahmed[i].images[0]}" class='w-100' style='height:350px'>
+                    <h4>${ahmed[i].title}</h4>
+                    <h5>${ahmed[i].category}</h5>
+                    <h5>${ahmed[i].price}$</h5>
+                    </div>
+                ` ; 
+       myText = myText+cartoona;
+
+}
+document.querySelector(".test").innerHTML=myText;
+})
+
+// ==============   End   fetch ===  then  =============
+
 
 
