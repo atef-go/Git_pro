fa416a4c0388ea340ac95e8726f68deffbdf2d23
atif <atifgo2022@gmail.com>
2025-06-05 00:09:06 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,81 @@
 
-// ========================= fetch ===    then   ===================
+// ============== The third method =============
 
- fetch("https://dummyjson.com/products")
-.then(function(items){
-    return items.json();
-})
-.then(function(result){
-// ==================================
-var ahmed =result.products;
-
-var myText = "";
-for(var i=0; i<ahmed.length; i++){
-
-    var cartoona =`
-                    <div class="col-md-4 text-center mb-5">
-                    <img src="${ahmed[i].images[0]}" class='w-100' style='height:350px'>
-                    <h4>${ahmed[i].title}</h4>
-                    <h5>${ahmed[i].category}</h5>
-                    <h5>${ahmed[i].price}$</h5>
-                    </div>
-                ` ; 
-       myText = myText+cartoona;
-
-}
-document.querySelector(".test").innerHTML=myText;
-})
-
-// ==============   End   fetch ===  then  =============
+(async function getApi(){
+    var items = await fetch("https://forkify-api.herokuapp.com/api/search?q=pizza");
+    var result = await items.json();
+    var ahmed =result.recipes;
+    var myText = "";
+    for(var i=0; i<ahmed.length; i++){
+        var cartoona =`
+                        <div class="col-md-4 text-center mb-5">
+                        <img src="${ahmed[i].image_url}" class='w-100' style='height:350px'>
+                        <h4>${ahmed[i].title}</h4>
+                        <h5>${ahmed[i].publisher}</h5>                    
+                        </div>
+                    ` ; 
+           myText = myText+cartoona;
+    }
+        document.querySelector(".test").innerHTML=myText;
+})();
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// ==============End  The third method =============
+  // console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("dem").innerHTML=result;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// https://jsonplaceholder.typicode.com/photos
+
+
+
+    // document.querySelector(".test img").src = result[0].url;
+    // document.querySelector(".test h4").innerHTML = result[0].title;
+    // document.querySelector(".test h5").innerHTML = result[0].id;
